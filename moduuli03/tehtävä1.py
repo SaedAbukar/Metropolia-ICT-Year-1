4b09ac92b91919c602f405321a232e8cba3d3135
SaedAbukar <saed.a.abukar@gmail.com>
2023-08-22 20:16:21 +0300

    Moduuli03 tehtävä1

---
+++ moduuli03/tehtävä1.py
@@ -0,0 +1,14 @@
+
+while True:
+    try:
+        kuhan_mitta = float(input('Mikä on kuhan pituus senttimetreinä? '))
+        break
+    except ValueError:
+        print('Et lisännyt lukua. Lisää kuhan pituus luvuina.')
+
+if kuhan_mitta < 37:
+    ali_jäämä = 37 - kuhan_mitta
+    print(f'Kuha on alamittainen. Alimmasta sallitusta pyyntimitasta puuttuu {ali_jäämä:.2f} cm')
+    print('Päästä kuha takaisin veteen.')
+else:
+    print('Hei, hieno pyydös!')
