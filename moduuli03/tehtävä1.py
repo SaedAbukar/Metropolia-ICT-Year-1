2d4455c5094e491d0d8f9f5323063cc48045844b
SaedAbukar <saed.a.abukar@gmail.com>
2023-08-23 21:52:08 +0300

    Moduuli03 tehtävä1

--- moduuli03/tehtävä1.py
+++ moduuli03/tehtävä1.py
@@ -7,8 +7,8 @@ while True:
         print('Et lisännyt lukua. Lisää kuhan pituus luvuina.')
 
 if kuhan_mitta < 37:
-    ali_jäämä = 37 - kuhan_mitta
-    print(f'Kuha on alamittainen. Alimmasta sallitusta pyyntimitasta puuttuu {ali_jäämä:.2f} cm')
+    ali_jaama = 37 - kuhan_mitta
+    print(f'Kuha on alamittainen. Alimmasta sallitusta pyyntimitasta puuttuu {ali_jaama:.2f} cm')
     print('Laske kuha takaisin järveen.')
 else:
     print('Hei, hieno pyydös!')
