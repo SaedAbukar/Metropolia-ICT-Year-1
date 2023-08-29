5d2e4e18ce80d490fe6a9f40dad25d13b991af7c
SaedAbukar <saed.a.abukar@gmail.com>
2023-08-29 11:17:38 +0300

    Moduuli04 tehtävä 6

---
+++ moduuli04/tehtävä6.py
@@ -0,0 +1,53 @@
+'''
+Toteutetaan algoritmi piin (π) likiarvon laskemiseksi. Oletetaan, että A on yksikköympyrä eli ympyrä,
+jonka keskipiste on origossa ja jonka säde on yksi. Sen ympärille piirretään pienin mahdollinen neliö B siten,
+että ympyrä A jää kokonaan neliön sisäpuolelle.
+Neliön nurkkapisteet ovat tällöin (-1,-1), (1,-1), (1,1) ja (-1,1).
+Jos neliön sisälle arvotaan satunnaisesti suuri määrä pisteitä,
+osuu niistä myös ympyrän sisälle likimain niin suuri osuus kuin ympyrän pinta-ala on neliön pinta-alasta
+eli πr^2/4, joka (koska ympyrän säde on yksi) sievenee muotoon π/4.
+Tästä saadaan yksinkertainen menetelmä piin likiarvon laskemiseksi:
+Arvotaan neliön B sisälle suuri määrä satunnaisissa kohdissa olevia pisteitä, esimerkiksi miljoona.
+Olkoon N tämä pisteiden kokonaismäärä. Jokaisesta neliön B sisään arvotusta pisteestä testataan vuorollaan,
+jääkö se myös yksikköympyrän A sisälle. Olkoon n ympyrän sisälle jäävien pisteiden kokonaismäärä.
+Nyt pätee n/N≈π/4, josta saadaan π≈4n/N.
+Kirjoita ohjelma, joka kysyy arvottavien pisteiden määrän käyttäjältä ja toteuttaa piin likiarvon laskennan
+edellä kuvatulla menetelmällä. Lopuksi ohjelma tulostaa piin likiarvon käyttäjälle.
+(Huomaa, että jokaisesta arvotusta pisteestä (x,y) on helppoa testata onko se yksikköympyrän A sisällä:
+riittää testata, toteuttaako piste epäyhtälön x^2+y^2<1.)
+'''
+
+import math
+import random
+
+# Kysytään käyttäjältä arvottavien pisteiden määrän.
+N = int(input('Lisää arvottavien pisteiden määrä: '))
+n = 0
+
+# Selvitetään ympyrän sisälle osuvien pisteiden määrä for loopilla ja tarkistetaan yhtälöllä,
+# että piste on ympyrän sisällä. Jos yhtälö toteutuu niin nostetaan ympyrän pisteiden määrää(n) yhdellä
+for _ in range(N):
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+
+    if x**2 + y**2 < 1:
+        n += 1
+
+# Lasketaan pii:n likiarvo
+estimated_pi = 4 * n / N
+print(f'Piin likiarvo: {estimated_pi}')
+
+
+
+
+
+
+
+
+
+
+# N = int(input('Lisää arvottavien pisteiden määrä: '))
+# n = math.pi * N / 4
+# print(n)
+# piin_likiarvo = 4 * n * N
+# print(f'Piin likiarvo on: {piin_likiarvo}')
