b170dd388f19ccbc7af4c88f1a077fc16ae33b93
SaedAbukar <saed.a.abukar@gmail.com>
2023-09-04 18:43:58 +0300

    Moduuli02

--- moduuli02/suorakulma.py
+++ moduuli02/suorakulma.py
@@ -13,4 +13,4 @@ while True:
 suorakulmion_piiri = suorakulmion_kanta * 2 + suorakulmion_korkeus * 2
 suorakulmion_pinta_ala = suorakulmion_kanta * suorakulmion_korkeus
 
-print(f'Suorakulmion piiri on: {suorakulmion_piiri:.2f} cm ja Suorakulmion pinta-ala on: {suorakulmion_pinta_ala:.2f} cm^2')
+print(f'Suorakulmion piiri on: {suorakulmion_piiri} cm ja Suorakulmion pinta-ala on: {suorakulmion_pinta_ala} cm^2')
