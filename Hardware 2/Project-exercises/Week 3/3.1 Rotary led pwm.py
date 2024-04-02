72b0f46afae22fb1fb71fb07c371c02dee1c68f8
SaedAbukar <saed.a.abukar@gmail.com>
2024-04-02 12:00:34 +0300

    Metropolia ICT Year 1

--- Hardware 2/Project-exercises/Week 3/3.1 Rotary led pwm.py
+++ Hardware 2/Project-exercises/Week 3/3.1 Rotary led pwm.py
@@ -14,82 +14,109 @@ class PWMLED:
         self.led.freq(frequency)
         self.brightness = 0
         self.set_brightness(self.brightness)
+        self.is_on = False
 
     def set_brightness(self, brightness):
-    
-        #:param brightness: Brightness level (0-10000)
+        """
+        Set the brightness of the LED.
         
+        :param brightness: Brightness level (0-10000).
+        """
         brightness = max(0, min(10000, brightness))  # Clamp brightness to 0-10000
         self.brightness = brightness
         duty = int((brightness / 10000) * 65535)
         self.led.duty_u16(duty)
        
     def increase_brightness(self, amount=100):
-
-        #:param amount: Amount to increase the brightness by (default is 10).
+        """
+        Increase the LED brightness by a specified amount.
         
+        :param amount: Amount to increase the brightness by (default is 100).
+        """
         new_brightness = self.brightness + amount
         self.set_brightness(new_brightness)
 
     def decrease_brightness(self, amount=100):
-
-        #:param amount: Amount to decrease the brightness by (default is 10).
+        """
+        Decrease the LED brightness by a specified amount.
         
+        :param amount: Amount to decrease the brightness by (default is 100).
+        """
         new_brightness = self.brightness - amount
         self.set_brightness(new_brightness)
 
+    def toggle(self):
+        """
+        Toggle the LED on/off.
+        """
+        if self.is_on:
+            self.off()
+        else:
+            self.on()
+
     def on(self):
+        """
+        Turn on the LED.
+        """
         self.set_brightness(self.brightness + 10)
+        self.is_on = True
 
     def off(self):
+        """
+        Turn off the LED.
+        """
         self.set_brightness(0)
+        self.is_on = False
 
 class Encoder:
-    
-    def __init__(self, rot_a, rot_b, rot_c):
-        self.a = Pin(rot_a, mode = Pin.IN, pull = Pin.PULL_UP)
-        self.b = Pin(rot_b, mode = Pin.IN, pull = Pin.PULL_UP)
-        self.c = Pin(rot_c, mode = Pin.IN, pull = Pin.PULL_UP)
-        self.pressed = False
+    def __init__(self, rot_a, rot_b):
+        self.a = Pin(rot_a, mode=Pin.IN, pull=Pin.PULL_UP)
+        self.b = Pin(rot_b, mode=Pin.IN, pull=Pin.PULL_UP)
         self.last_pressed_time = 0
-        self.fifo = Fifo(30, typecode = 'i')
-        self.a.irq(handler = self.handler, trigger = Pin.IRQ_RISING, hard = True)
-        self.c.irq(handler = self.switch, trigger = Pin.IRQ_FALLING, hard = True)
-        
+        self.fifo = Fifo(30, typecode='i')
+        self.a.irq(handler=self.handler, trigger=Pin.IRQ_RISING, hard=True)
+
     def handler(self, pin):
-        if self.b():
+        if self.b.value():
             self.fifo.put(-1)
         else:
             self.fifo.put(1)
-    
-    def switch(self, pin):
-        # Debouncing using time.ticks_ms()
-        current_time = time.ticks_ms()
-        if current_time - self.last_pressed_time < 200:  # Adjust debounce time as needed (200ms in this case)
-            return
-        self.last_pressed_time = current_time
+
+class Button:
+    def __init__(self, pin_number):
+        """
+        Initialize the button on a specific pin.
         
-        if self.pressed:
-            self.fifo.put(3)
-            self.pressed = False
-        else:
-            self.fifo.put(2)
-            self.pressed = True
+        :param pin_number: Pin number where the button is connected.
+        """
+        self.button = Pin(pin_number, Pin.IN, Pin.PULL_UP)
+        self.last_pressed_time = 0
 
-rot = Encoder(10, 11, 12)
+    def is_pressed(self):
+        """
+        Check if the button is pressed (with debouncing).
+        
+        :return: True if the button is pressed, False otherwise.
+        """
+        current_time = time.ticks_ms()
+        if current_time - self.last_pressed_time < 200:  # Debounce time (200ms)
+            return False
+        if self.button.value() == 0:
+            self.last_pressed_time = current_time
+            return True
+        return False
 
+rot = Encoder(10, 11)
+button = Button(12)
 led = PWMLED(22)
 
 while True:
+    if button.is_pressed():
+        led.toggle()
+
     if rot.fifo.has_data():
         value = rot.fifo.get()
-        print(value)
-        print(rot.pressed)
-        if rot.pressed:
-            led.on()
-            if value == 1:
-                led.increase_brightness()
-            if value == -1:
-                led.decrease_brightness()
-        else:
-            led.off()
+        if value == 1:
+            led.increase_brightness()
+        elif value == -1:
+            led.decrease_brightness()
